227ca15f77e5299c31a1a4c6f188f4f66fe4b899
bethschoenfeld <bethingraham3@gmail.com>
2018-06-01 20:42:27 -0400

    Have a basic site up and running

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import './index.css'
+ReactDOM.render((
+  <Router>
+    <App />
+  </Router>
+), document.getElementById('root'))
+registerServiceWorker()
