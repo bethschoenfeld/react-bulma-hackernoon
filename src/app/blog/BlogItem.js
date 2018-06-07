39966d2bd841763186039597fa6e745c5f70bcee
bethschoenfeld <bethingraham3@gmail.com>
2018-06-06 22:30:21 -0400

    broke up shared pieces of presentational components

--- src/app/blog/BlogItem.js
+++ src/app/blog/BlogItem.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import BlogNav from './shared/BlogNav'
+import BlogContent from './shared/BlogContent'
 
 
 const BlogItem = (props) => (
-  <div className="box content">
-    <h1>{props.title}</h1>
-    <p>{props.content}</p>
+  <div className="box">
+    <BlogContent limit={150} {...props}>
+      <BlogNav date={props.date}
+        status={props.status}
+        to={{
+          pathname: `/blog/${props.path}`,
+          state: { props }
+
+        }} />
+    </BlogContent>
   </div>
 )
-
 export default BlogItem
